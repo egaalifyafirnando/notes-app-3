a60b0cbc0eb3e377f77e049e5c40d594fa043827
Ega Alifya Firnando <egaalifyaf@gmail.com>
2023-07-19 19:25:11 +0700

    update: migrate to hooks

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { getUserLogged, putAccessToken } from '../utils/api';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import { LocaleProvider } from '../contexts/LocaleContext';
 import Navigation from './Navigation';
@@ -9,117 +10,93 @@ import DetailPage from '../pages/DetailPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import RegisterPage from '../pages/RegisterPage';
 import LoginPage from '../pages/LoginPage';
-import { getUserLogged, putAccessToken } from '../utils/api';
 
-class NoteApp extends React.Component {
-    constructor(props) {
-        super(props);
+const NoteApp = () => {
+    const [authedUser, setAuthedUser] = useState(null);
+    const [initializing, setInitializing] = useState(true);
+    const [theme, setTheme] = useState(
+        () => localStorage.getItem('theme') || 'light'
+    );
+    const [locale, setLocale] = useState(
+        () => localStorage.getItem('locale') || 'id'
+    );
 
-        this.state = {
-            authedUser: null,
-            initializing: true,
-            theme: localStorage.getItem('theme') || 'light',
-            toggleTheme: () => {
-                this.setState((prevState) => {
-                    const newTheme =
-                        prevState.theme === 'light' ? 'dark' : 'light';
-                    localStorage.setItem('theme', newTheme);
-                    return {
-                        theme: newTheme,
-                    };
-                });
-            },
-            localeContext: {
-                locale: localStorage.getItem('locale') || 'id',
-                toggleLocale: () => {
-                    this.setState((prevState) => {
-                        const newLocale =
-                            prevState.localeContext.locale === 'id'
-                                ? 'en'
-                                : 'id';
-                        localStorage.setItem('locale', newLocale);
-                        return {
-                            localeContext: {
-                                ...prevState.localeContext,
-                                locale: newLocale,
-                            },
-                        };
-                    });
-                },
-            },
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        localStorage.setItem('theme', newTheme);
+        setTheme(newTheme);
+    };
+
+    const toggleLocale = () => {
+        const newLocale = locale === 'id' ? 'en' : 'id';
+        localStorage.setItem('locale', newLocale);
+        setLocale(newLocale);
+    };
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    useEffect(() => {
+        const fetchUserLogged = async () => {
+            const { data } = await getUserLogged();
+            setAuthedUser(data);
+            setInitializing(false);
         };
 
-        this.onLoginSuccess = this.onLoginSuccess.bind(this);
-        this.onLogout = this.onLogout.bind(this);
-    }
+        fetchUserLogged();
+    }, []);
 
-    async componentDidMount() {
-        document.documentElement.setAttribute('data-theme', this.state.theme);
-        const { data } = await getUserLogged();
-        this.setState(() => {
-            return {
-                authedUser: data,
-                initializing: false,
-            };
-        });
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.theme !== this.state.theme) {
-            document.documentElement.setAttribute(
-                'data-theme',
-                this.state.theme
-            );
-        }
-    }
-
-    async onLoginSuccess({ accessToken }) {
+    const onLoginSuccess = async ({ accessToken }) => {
         putAccessToken(accessToken);
         const { data } = await getUserLogged();
+        setAuthedUser(data);
+    };
 
-        this.setState(() => {
-            return {
-                authedUser: data,
-            };
-        });
-    }
-
-    onLogout() {
-        this.setState(() => {
-            return {
-                authedUser: null,
-            };
-        });
-
+    const onLogout = () => {
+        setAuthedUser(null);
         putAccessToken('');
+    };
+
+    const themeContextValue = useMemo(() => ({ theme, toggleTheme }), [theme]);
+    const localeContextValue = useMemo(
+        () => ({ locale, toggleLocale }),
+        [locale]
+    );
+
+    if (initializing) {
+        return null;
     }
 
-    render() {
-        if (this.state.initializing) {
-            return null;
-        }
-
-        if (this.state.authedUser === null) {
-            return (
-                <ThemeProvider value={this.state}>
-                    <LocaleProvider value={this.state.localeContext}>
-                        <div className='note-app'>
-                            <header className='note-app__header'>
-                                <h1>
-                                    {this.state.localeContext.locale === 'id'
-                                        ? 'Aplikasi Catatan'
-                                        : 'Notes App'}
-                                </h1>
-                            </header>
-                            <main>
-                                <Routes>
+    return (
+        <ThemeProvider value={themeContextValue}>
+            <LocaleProvider value={localeContextValue}>
+                <div className='note-app'>
+                    <header className='note-app__header'>
+                        <h1>
+                            {locale === 'id' ? 'Aplikasi Catatan' : 'Notes App'}
+                        </h1>
+                        {authedUser !== null && (
+                            <>
+                                <Navigation logout={onLogout} />
+                                <p
+                                    style={{ fontSize: '1.5rem' }}
+                                    className='grey'
+                                >
+                                    Hi, {authedUser.name}
+                                </p>
+                            </>
+                        )}
+                    </header>
+                    <main>
+                        <Routes>
+                            {authedUser === null ? (
+                                <>
                                     <Route
                                         path='/*'
                                         element={
                                             <LoginPage
-                                                loginSuccess={
-                                                    this.onLoginSuccess
-                                                }
+                                                loginSuccess={onLoginSuccess}
                                             />
                                         }
                                     />
@@ -127,48 +104,30 @@ class NoteApp extends React.Component {
                                         path='/register'
                                         element={<RegisterPage />}
                                     />
-                                </Routes>
-                            </main>
-                        </div>
-                    </LocaleProvider>
-                </ThemeProvider>
-            );
-        }
-
-        return (
-            <ThemeProvider value={this.state}>
-                <LocaleProvider value={this.state.localeContext}>
-                    <div className='note-app'>
-                        <header className='note-app__header'>
-                            <h1>
-                                {this.state.localeContext.locale === 'id'
-                                    ? 'Aplikasi Catatan'
-                                    : 'Notes App'}
-                            </h1>
-                            <Navigation logout={this.onLogout} />
-                            <p style={{ fontSize: '1.5rem' }} className='grey'>
-                                Hi, {this.state.authedUser.name}
-                            </p>
-                        </header>
-                        <main>
-                            <Routes>
-                                <Route path='/' element={<HomePage />} />
-                                <Route
-                                    path='/notes/new'
-                                    element={<AddPage />}
-                                />
-                                <Route path='*' element={<NotFoundPage />} />
-                                <Route
-                                    path='/notes/:id'
-                                    element={<DetailPage />}
-                                />
-                            </Routes>
-                        </main>
-                    </div>
-                </LocaleProvider>
-            </ThemeProvider>
-        );
-    }
-}
+                                </>
+                            ) : (
+                                <>
+                                    <Route path='/' element={<HomePage />} />
+                                    <Route
+                                        path='/notes/new'
+                                        element={<AddPage />}
+                                    />
+                                    <Route
+                                        path='*'
+                                        element={<NotFoundPage />}
+                                    />
+                                    <Route
+                                        path='/notes/:id'
+                                        element={<DetailPage />}
+                                    />
+                                </>
+                            )}
+                        </Routes>
+                    </main>
+                </div>
+            </LocaleProvider>
+        </ThemeProvider>
+    );
+};
 
 export default NoteApp;
